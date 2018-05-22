c2046fb7e971e195b201e3f8b5565708becdc5d5
sfshahi <38735094+sfshahi@users.noreply.github.com>
2018-05-21 22:09:41 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -20,7 +20,9 @@
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
                               'http://loinc.org|3141-9', 'http://loinc.org|85353-1',
                               'http://loinc.org|9279-1', 'http://loinc.org|59408-5',
-                              'http://loinc.org|8867-4', 'http://loinc.org|8310-5', ]
+                              'http://loinc.org|8867-4', 'http://loinc.org|8310-5', 
+                              'http://loinc.org|2951-2', 'http://loinc.org|2823-3', 
+                             ]
                       }
                     }
                   });
@@ -55,8 +57,8 @@
           var temp = byCodes('8310-5');
           //var gcs = byCodes('9269-2');
           
-          //var sodium = byCodes('24323-8');
-          //var potassium = byCodes('2823-3');
+          var sodium = byCodes('2951-2');
+          var potassium = byCodes('2823-3');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
 
@@ -83,8 +85,8 @@
             p.diastolicbp = diastolicbp;
           }
 
-          //p.sodium = getQuantityValueAndUnit(sodium[0]);
-          //p.potassium = getQuantityValueAndUnit(potassium[0]);
+          p.sodium = getQuantityValueAndUnit(sodium[0]);
+          p.potassium = getQuantityValueAndUnit(potassium[0]);
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
 
@@ -117,8 +119,8 @@
       pulseox: {value: ''},
       temp: {value: ''},
       //gcs: {value: ''},
-      //sodium: {value: ''},
-      //potassium: {value: ''},
+      sodium: {value: ''},
+      potassium: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
     };
@@ -191,8 +193,8 @@
     $('#pulseox').html(p.pulseox);
     $('#temp').html(p.temp);
     //$('#gcs').html(p.gcs);
-    //$('#sodium').html(p.sodium);
-    //$('#potassium').html(p.potassium);
+    $('#sodium').html(p.sodium);
+    $('#potassium').html(p.potassium);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
   };
